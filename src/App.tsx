245a997b2209359225af526fc664788f94e1915f
Alexander Kramer <lxkrmr@icloud.com>
2023-03-20 11:07:15 +0100

    ✨ router basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
 import ProjectsPage from "./projects/ProjectsPage";
+import HomePage from "./home/HomePage";
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="container">
-      <ProjectsPage />
-    </div>
+    <Router>
+      <header className="sticky">
+        <span className="logo">
+          <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
+        </span>
+        <NavLink to="/" className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/projetcs" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/projetcs" element={<ProjectsPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
